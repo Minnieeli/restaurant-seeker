0ca1365f41f99f57e2474531e6f847cc2b97b782
Min Li <min680069@gmail.com>
2020-11-17 15:45:29 -0800

    first ui commit

--- Restaurant Seeker/ContentView.swift
+++ Restaurant Seeker/ContentView.swift
@@ -9,8 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, foodies!")
-            .padding()
+        VStack {
+            Text("Hello, foodies!")
+                .padding()
+            
+        }
     }
 }
 
