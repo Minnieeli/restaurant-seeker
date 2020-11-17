626ab7eaa7338db38b94021aaf364002781f5d68
Min Li <min680069@gmail.com>
2020-11-17 11:35:20 -0800

    initial commit

---
+++ Restaurant Seeker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Restaurant Seeker
+//
+//  Created by Minnie on 11/17/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Restaurant Seeker/Restaurant_SeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Restaurant_SeekerApp.swift
+//  Restaurant Seeker
+//
+//  Created by Minnie on 11/17/20.
+//
+
+import SwiftUI
+
+@main
+struct Restaurant_SeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
